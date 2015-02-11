24c5940575412dd8a44e088dc994bd1aeb72ef8b
Sten Verbois <stenverbois@gmail.com>
2015-02-11 15:59:54 +0100

    Add Zurb Foundation

---
+++ l2p/foundation/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+
+# Test patern, renders Zurb Foundation default page using base template
+urlpatterns = patterns('',
+    url(regex=r'^$',
+        view=TemplateView.as_view(template_name="foundation/index.html"),
+        name="foundation_index"),
+
+    url(regex=r'^icons/$',
+        view=TemplateView.as_view(template_name="foundation/icons.html"),
+        name="foundation_icons"),
+)
--- l2p/l2p/urls.py
+++ l2p/l2p/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from django.contrib import admin
 
 urlpatterns = patterns('',
@@ -7,4 +8,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    #url(r'^date/', 'l2p.views.date', name='date')
+
+    # Include Zurb Foundation test pages
+    url(r'zurb/', include('foundation.urls'))
 )
