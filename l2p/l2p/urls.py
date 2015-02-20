a58664122319acf91db0e540438d684c80fe2c34
Sten Verbois <stenverbois@gmail.com>
2015-02-20 09:52:57 +0100

    Add user and group placeholder page

--- l2p/l2p/urls.py
+++ l2p/l2p/urls.py
@@ -13,10 +13,15 @@ urlpatterns = patterns('',
     # TODO Further templating on the foundations html for more generic code?
     url(r'^$', 'l2p.views.home', name='home'),
 
-    url(r'^info/', 'l2p.views.info', name='info'),
+    # User page (/u/<id>)
+    url(r'^u/(?P<id>\d*)/$', 'l2p.views.user'),
 
-    # Include Zurb Foundation test pages
+    # Group page (/g/<id>)
+    url(r'^g/(?P<id>\d*)/$', 'l2p.views.group'),
+
+    # Zurb Foundation test pages
     url(r'zurb/', include('foundation.urls')),
 
-    #url(r'^polls/', include('polls.urls', namespace="polls"))
+    # TESTING
+    url(r'^info/', 'l2p.views.info'),
 )
