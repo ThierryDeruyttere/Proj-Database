43eed676c9a2af4106b302a8de00c9576a97446d
ThierryDeruyttere <thierryderuyttere@hotmail.com>
2015-02-22 16:01:18 +0100

    Added insert user

--- l2p/dbw/__init__.py
+++ l2p/dbw/__init__.py
@@ -14,9 +14,21 @@ def getAll(table):
     return dictfetchall()
 
 def getUser(username):
-	cursor.execute("SELECT id, first_name, password FROM user WHERE user.first_name = " + "'%s'" % username)
-	return dictfetchall()
+    cursor.execute("SELECT id, first_name, password FROM user WHERE user.first_name = " + "'%s'" % username)
+    return dictfetchall()
 
 def getUserInformation(id):
-	cursor.execute("SELECT * FROM user WHERE user.id = " + "'%s'" % id)
-	return dictfetchall()
+    cursor.execute("SELECT * FROM user WHERE user.id = " + "'%s'" % id)
+    return dictfetchall()
+
+def insertIntoTable(tableName, **kwargs):
+    columnNames = []
+    for key in kwargs.keys():
+        columnNames.append('%(' + key + ')s')
+
+    queryColumns = ', '.join(kwargs.keys())
+    queryValues = ', '.join(columnNames)
+    cursor.execute("INSERT INTO " + tableName + "("+ queryColumns  +") VALUES ("+ queryValues +");", kwargs)
+
+def insertUser(first_name, last_name, password, email, is_active = 1):
+    cursor.execute("INSERT INTO user(is_active, first_name, last_name, password, email) VALUES ({}, {} , {}, {}, {});".format(is_active, first_name,last_name,password,email))
