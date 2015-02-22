46175e76924e53ff9081d176a124a8dfc09c7862
Sten Verbois <stenverbois@gmail.com>
2015-02-22 12:24:59 +0100

    Fix homepage

--- l2p/l2p/views.py
+++ l2p/l2p/views.py
@@ -8,21 +8,7 @@ from om import *
 object_manager = objectmanager.ObjectManager()
 
 def home(request):
-    # Get choices
-    choices = dbw.getAll('polls_choice')
-
-    # Get questions
-    questins = dbw.getAll('polls_question')
-
-    # Make list of pairs (question, choice_list)
-    l = []
-    for question in questions:
-        item = {}
-        item['question_text'] = question['question_text']
-        item['choices'] = [{'choice_text':choice['choice_text']} for choice in choices if choice['question_id'] == question['id']]
-        l.append(item)
-
-    return render(request, 'question/index.html', {'test':'haha', 'question_list':l})
+    return render(request, 'home.html', {})
 
 def user(request, id = 0):
     return render(request, 'user.html', {'id':id})
@@ -45,9 +31,3 @@ def list(request, id = 0):
 
 def question(request, id, question):
     return render(request, 'question.html', {})
-
-# TESTING
-def info(request):
-    links = []
-    links.append("Index Page")
-    return render(request, 'question/info.html', {'text1':'FTW','text2':'FTL','links':links})
