cabd28a7eb32079b089c3bfa0d8c4b06ef00fae3
Sten Verbois <stenverbois@gmail.com>
2015-02-26 15:59:54 +0100

    Fix identation error in views.py

--- l2p/l2p/views.py
+++ l2p/l2p/views.py
@@ -1,4 +1,4 @@
-    from django.http import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password
 import hashlib
