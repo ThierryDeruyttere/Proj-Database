a6f5af50a4777693a08d77b360bb1fc4d1f13e20
ThierryDeruyttere <thierryderuyttere@hotmail.com>
2015-03-03 20:57:33 +0100

    Starting with code exercise => can now escape escape characters

--- codegalaxy/codegalaxy/exercises/views.py
+++ codegalaxy/codegalaxy/exercises/views.py
@@ -13,6 +13,8 @@ from pymysql import escape_string
 
 object_manager = objectmanager.ObjectManager()
 
+
+
 @require_login
 def createExerciseList(request):
     languages = dbw.getAll("programmingLanguage")
