05ec1547ed872ca77a2ab2248f38e5021d3e3fcb
Sten Verbois <stenverbois@gmail.com>
2015-03-12 20:43:43 +0100

    Working footer links

--- codegalaxy/codegalaxy/views.py
+++ codegalaxy/codegalaxy/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.template import RequestContext
 from django.shortcuts import render, redirect
 from django.utils.translation import ugettext as _
 from django.contrib.auth.hashers import make_password
