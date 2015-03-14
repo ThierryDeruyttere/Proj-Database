cd5a7a3ec176bd3f45d45814a5829bc4f303e61f
ThierryDeruyttere <thierryderuyttere@hotmail.com>
2015-03-14 20:35:47 +0100

    Removing prints

--- codegalaxy/custom_filters/templatetags/custom_filters.py
+++ codegalaxy/custom_filters/templatetags/custom_filters.py
@@ -10,11 +10,8 @@ def get_range(number):
 @register.assignment_tag(takes_context=True)
 def check_loggedIn(context):
     try:
-        print("checking")
         if authentication.logged_user(context['request']) is None:
-            print("false")
             return False
-        print("true")
         return True
     except Exception as e:
         return False
