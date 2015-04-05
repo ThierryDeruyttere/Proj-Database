660fb1cb6357ca7a3c24bc88048aaf61f7e7b5d8
stenverbois <stenverbois@gmail.com>
2015-04-05 15:05:24 +0200

    Baseclass FeedItem for feed items

--- codegalaxy/managers/om/feed.py
+++ codegalaxy/managers/om/feed.py
@@ -8,54 +8,46 @@ import datetime
 
 import os.path
 
-class UserInGroup:
+class FeedItem:
+    def __init__(self, type, time_stamp):
+        self.type = type
+        self.datetime = time_stamp
+
+    def __repr__(self):
+        return str(self)
+
+class UserInGroup(FeedItem):
     def __init__(self, group, user, user_permissions, joined_on, status):
+        super().__init__('UserInGroup', joined_on)
+
         self.group = group
         self.user = user
         self.user_permissions = user_permissions
         self.joined_on = joined_on
         self.status = status
 
-        self.datetime = joined_on
-
-        self.type = "UserInGroup"
-
     def __str__(self):
         return self.user.first_name + ' ' + self.user.last_name + ' became member of ' + self.group.group_name + ' on ' + str(self.joined_on)
 
-    def __repr__(self):
-        return str(self)
-
-
-class MadeExerciseList:
+class MadeExerciseList(FeedItem):
     def __init__(self, user, exercise_list, completed_on):
+        super().__init__('MadeExerciseList', completed_on)
+
         self.user = user
         self.exercise_list = exercise_list
         self.completed_on = completed_on
 
-        self.datetime = completed_on
-
-        self.type = "MadeExerciseList"
-
     def __str__(self):
         return self.user.first_name + ' ' + self.user.last_name + ' made: ' + self.exercise_list.name + ' on ' + str(self.completed_on)
 
-    def __repr__(self):
-        return str(self)
-
-class FriendsWith:
+class FriendsWith(FeedItem):
     def __init__(self, user, friend, befriended_on, status):
+        super().__init__('FriendsWith', befriended_on)
+
         self.user = user
         self.friend = friend
         self.befriended_on = befriended_on
         self.status = status
 
-        self.datetime = befriended_on
-
-        self.type = "FriendsWith"
-
     def __str__(self):
         return self.user.first_name + ' ' + self.user.last_name + ' became friends with: ' + self.friend.first_name + ' ' + self.friend.last_name + ' on ' + str(self.befriended_on)
-
-    def __repr__(self):
-        return str(self)
