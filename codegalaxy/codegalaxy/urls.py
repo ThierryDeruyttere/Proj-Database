38371f499e1b6ccf411a1ef344ae081dacdac5e1
stenverbois <stenverbois@gmail.com>
2015-04-19 15:30:22 +0200

    Fix and cleanup AnswerQuestion

--- codegalaxy/codegalaxy/urls.py
+++ codegalaxy/codegalaxy/urls.py
@@ -57,7 +57,7 @@ urlpatterns += patterns('codegalaxy.exercises.views',
     # Submit exercise answer (/l/<id>/<question>/submit)
     url(r'^l/(?P<list_id>\d+)/(?P<exercise_number>\d+)/submit$', 'submit'),
 
-    url(r'^l/(?P<listId>\d*)/(?P<exercise_id>\d*)/(?P<exercise_number>\d*)/editExercise/$', 'editExercise'),
+    url(r'^l/(?P<listId>\d*)/(?P<exercise_id>\d*)/(?P<exercise_number>\d*)/edit/$', 'editExercise'),
 
     url(r'^l/(?P<listId>\d*)/importExercise/$', 'importExercise'),
     url(r'^l/(?P<listId>\d*)/editList/$', 'editList'),
