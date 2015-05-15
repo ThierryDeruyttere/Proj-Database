6bb3dbb6beb4c59289d13cae6882a4a6dcb16796
tristanvandeputte <tristanvandeputte@hotmail.com>
2015-05-15 20:03:08 +0200

    Added markdown to wall, updated README

--- codegalaxy/managers/om/group.py
+++ codegalaxy/managers/om/group.py
@@ -3,10 +3,11 @@ import managers.om.user
 import managers.om.objectmanager
 import datetime
 import time
-
+import markdown2
 
 import os.path
 
+markdown_converter = markdown2.Markdown()
 
 class Group:
 
@@ -262,7 +263,7 @@ class Post:
     def HTMLBasic(self, user, logged_user):
         # TODO: octicons adden voor buttons
         html = ''
-        html += '<p>' + self.post_text + '</p>'
+        html += '<p>' + markdown_converter.convert(self.post_text) + '</p>'
         html += '<p class="feed-timestamp"><small><span class="octicon octicon-clock"></span>' + str(self.posted_on)[:-6] + ' by ' + user.name() + '</small></p>'
         #want_to_reply_button
         html += '<div class="row">'
